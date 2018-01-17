8e751d169f785604a748bb276f89ebad241f8e45
ematt44 <ericmatthews133@gmail.com>
2018-01-17 15:14:59 -0800

    file set up

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO cats (burger_name, devoured) VALUES ('Double Double', true);
+INSERT INTO cats (burger_name, devoured) VALUES ('Whopper', true);
+INSERT INTO cats (burger_name, devoured) VALUES ('Big Mac', true);
